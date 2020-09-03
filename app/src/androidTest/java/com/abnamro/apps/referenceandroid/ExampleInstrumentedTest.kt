f81c45ae2e5d7514ab84082d352cb48e9cd20f21
Krabik <cheshirekrab@gmail.com>
2020-09-03 20:00:48 +0200

    Initial autotest

--- app/src/androidTest/java/com/abnamro/apps/referenceandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/abnamro/apps/referenceandroid/ExampleInstrumentedTest.kt
@@ -1,12 +1,14 @@
 package com.abnamro.apps.referenceandroid
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,8 +18,11 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
-
+        onView(withText("Hello World!")).check(matches(isDisplayed()))
     }
 }
